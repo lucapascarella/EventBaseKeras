9fe96ad20ed55c0c03b3c202d8e5fb07a7030076
Luca Pascarella <luca.pascarella@gmail.com>
2022-10-17 19:51:07 +0200

    Initial commit

---
+++ DataGenerator.py
@@ -0,0 +1,215 @@
+import os
+import re
+import json
+
+import numpy as np
+
+from unipath import Path
+import tensorflow
+from keras import backend
+from keras import Sequential
+from keras.utils import Sequence
+from keras.layers import RandomFlip, RandomRotation
+from typing import Tuple
+
+import img_utils
+
+
+class MyInput:
+    def __init__(self, filename: str, steering: float, torque: float, engine_velocity: float, vehicle_velocity: float, frame_number: int):
+        self.filename = filename
+        self.steering = steering
+        self.torque = torque
+        self.engine_velocity = engine_velocity
+        self.vehicle_velocity = vehicle_velocity
+        self.frame_number = frame_number
+        self.output = None
+
+
+class CustomSequence(Sequence):
+    """
+        Class for runtime image loading
+        It assumes that the input folder structure is:
+        root_folder/
+               folder_1/
+                        dvs/ aps/ aps_diff/
+                        sync_steering
+               folder_2/
+                        dvs/ aps/ aps_diff/
+                        sync_steering
+               .
+               .
+               folder_n/
+                        dvs/ aps/ aps_diff/
+                        sync_steering
+        """
+
+    def __init__(self, input_dir_data: str, frame_mode: str = 'dvs', is_training: bool = False, image_size: Tuple[int, int] = (224, 224), batch_size: int = 32,
+                 shuffle: bool = True, seed: int = None, follow_links: bool = False):
+        # Initialization
+        self.is_training = is_training
+        self.image_size = image_size
+        self.crop_size = image_size
+        self.img_channels = 3
+        self.batch_size = batch_size
+        self.seed = seed
+        self.shuffle = shuffle
+        self.follow_links = follow_links
+
+        # Check that the given frame is supported
+        if frame_mode not in {'dvs', 'aps', 'aps_diff'}:
+            raise ValueError('Invalid frame mode:', frame_mode, '; expected "dvs", "aps", or "aps_diff".')
+        self.frame_mode = frame_mode
+
+        self.scaler_json = os.path.join(Path(input_dir_data).parent, 'scaler.json')
+
+        if self.frame_mode == 'dvs':
+            try:
+                self.event_percentiles = np.loadtxt(os.path.join(Path(input_dir_data).parent, 'percentiles.txt'), usecols=0, skiprows=1)
+            except IOError:
+                raise IOError("Percentiles file not found")
+        else:
+            self.event_percentiles = None
+
+        # Get a list of input data folders
+        expected_files = 0
+        self.input_data = []
+        for sub_dir_level_one in sorted(os.listdir(input_dir_data)):
+            sub_dir_level_one = os.path.join(input_dir_data, sub_dir_level_one)
+            if os.path.isdir(sub_dir_level_one):
+
+                # Check if input data are organized properly in the required sub folder structure
+                steering_filename = os.path.join(sub_dir_level_one, 'sync_steering.txt')
+                if {'dvs', 'aps', 'aps_diff'}.issubset(os.listdir(sub_dir_level_one)) and os.path.isfile(steering_filename):
+                    steering = np.loadtxt(steering_filename, delimiter=',', skiprows=1)
+                    expected_files += steering.shape[0]
+
+                    sub_dir_level_two = os.path.join(sub_dir_level_one, self.frame_mode)
+                    unsorted_files = []
+                    for file in os.listdir(sub_dir_level_two):
+                        tmp = os.path.join(sub_dir_level_two, file)
+                        if os.path.isfile(tmp) and tmp.endswith('.png'):
+                            unsorted_files.append(tmp)
+
+                    # Get an ordered list of frames
+                    sorted_files = sorted(unsorted_files, key=lambda fil: int(re.search(r'_(\d+)\.png', fil).group(1)))
+
+                    # Be sure no other files other than PNGs are in the given sub folder
+                    if steering.shape[0] == len(sorted_files):
+                        for index, filename in enumerate(sorted_files):
+                            offset = 6  # This is more or less 1/3s in the future!
+                            if index + offset < steering.shape[0]:
+                                # Filter those images whose velocity is under 23 km/h and 30% of images whose steering is under 5 (for training)
+                                if self.is_training and (np.abs(steering[index][3]) < 2.3e1 or (np.abs(steering[index + offset][0]) < 5.0 and np.random.random() < 0.3)):
+                                    self.input_data.append(MyInput(filename, steering[index][0], steering[index][1], steering[index][2], steering[index][3], index))
+                                # Filter those images whose velocity is under 15 km/h (for evaluation)
+                                if not self.is_training and np.abs(steering[index][3]) < 1.5e1:
+                                    self.input_data.append(MyInput(filename, steering[index][0], steering[index][1], steering[index][2], steering[index][3], index))
+                    else:
+                        raise ValueError("Mismatch between frames in {} and rows in ".format(sub_dir_level_two, steering_filename))
+                else:
+                    raise ValueError("Invalid frame/folder structure:")
+
+        # Normalize output/prediction
+        outputs = self._output_normalization(np.array([i.steering for i in self.input_data], dtype=backend.floatx()))
+        for idx, out in enumerate(outputs):
+            self.input_data[idx].output = out
+
+        # Used to emulate ImageDataGenerator => random_transform(x) and standardize(x)
+        self.data_augmentation = Sequential([
+            RandomFlip('horizontal'),
+            RandomRotation(0.2),
+        ])
+
+        self.samples = len(self.input_data)
+        print("Selected {} of {} frames for {}".format(self.samples, expected_files, 'training' if is_training else 'validation'))
+        self.indexes = np.arange(self.samples)
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def _recursive_list(self, sub_path):
+        return sorted(os.walk(sub_path, followlinks=self.follow_links), key=lambda tpl: tpl[0])
+
+    def _output_normalization(self, outputs: np.array) -> np.array:
+        """
+        Normalize input array between -1 and 1.
+
+        # Arguments
+            array: input array.
+            directory:
+
+        # Returns
+            array: normalized array.
+        """
+        if self.is_training:
+            # Get mean and std dev
+            means = np.mean(outputs)
+            stds = np.std(outputs)
+            # 3sigma clipping
+            outputs = np.clip(outputs, means - 3 * stds, means + 3 * stds)
+            # Get min and max before scaling
+            clip_min = np.min(outputs)
+            clip_max = np.max(outputs)
+
+            # Scaling all values
+            outputs /= np.max(np.abs(outputs), axis=0)
+
+            out_dict = {'means': means.tolist(),
+                        'stds': stds.tolist(),
+                        'mins': clip_min.tolist(),
+                        'maxs': clip_max.tolist()}
+
+            # Save dictionary for later testing
+            with open(self.scaler_json, 'w') as f:
+                json.dump(out_dict, f)
+
+        else:
+            # Read dictionary
+            with open(self.scaler_json, 'r') as f:
+                train_dict = json.load(f)
+
+            # 3sigma clipping
+            means = train_dict['means']
+            stds = train_dict['stds']
+            outputs = np.clip(outputs, means - 3 * stds, means + 3 * stds)
+
+            # Scaling of all values
+            mins = np.array(train_dict['mins'])
+            maxs = np.array(train_dict['maxs'])
+
+            # Range of the transformed data
+            min_bound = -1.0
+            max_bound = 1.0
+
+            outputs = (outputs - mins) / (maxs - mins)
+            outputs = outputs * (max_bound - min_bound) + min_bound
+
+        return outputs
+
+    def __getitem__(self, index):
+        # get batch indexes from shuffled indexes
+        batch_indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        batch_x = np.zeros((self.batch_size,) + self.image_size + (self.img_channels,), dtype=backend.floatx())
+        batch_y = np.zeros((self.batch_size, 1), dtype=backend.floatx())
+
+        for i, batch_index in enumerate(batch_indexes):
+            batch_x[i] = img_utils.load_img(self.input_data[batch_index].filename, self.frame_mode, self.event_percentiles, self.image_size, self.crop_size)
+            batch_y[i] = self.input_data[batch_index].output
+            # print("File {} steering {} {}".format(self.input_data[batch_index].filename, self.input_data[batch_index].steering, self.input_data[batch_index].output))
+
+        # Emulate ImageDataGenerator => random_transform(x) and standardize(x)
+        data = tensorflow.data.Dataset.from_tensor_slices(batch_x)
+        data.map(lambda x: self.data_augmentation(x))
+
+        return batch_x, batch_y
+
+    def __len__(self):
+        # Denotes the number of batches per epoch
+        return self.samples // self.batch_size
+
+    def on_epoch_end(self):
+        # Updates indexes after each epoch
+        self.indexes = np.arange(self.samples)
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
---
+++ main.py
@@ -0,0 +1,173 @@
+import argparse
+import os
+
+import log_utils
+import DataGenerator
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Model
+from keras import backend
+from keras.callbacks import ModelCheckpoint
+from keras.applications import ResNet50
+from keras.layers import Dense
+from keras.layers import GlobalAveragePooling2D
+
+
+def build_model(img_width: int, img_height: int, img_channels: int, output_dim: int) -> Model:
+    img_input = keras.Input(shape=(img_width, img_height, img_channels))
+    print(img_input.shape)
+
+    base_model = ResNet50(input_tensor=img_input, weights=None, include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+
+    x = Dense(1024, activation='relu')(x)
+
+    # Steering channel
+    output = Dense(output_dim)(x)
+
+    model = Model(inputs=[img_input], outputs=[output])
+    # print(model.summary())
+
+    return model
+
+
+def steering_loss(y_true, y_pred):
+    return tf.reduce_mean(backend.square(y_pred - y_true))
+
+
+def pred_std(y_true, y_pred):
+    _, var = tf.nn.moments(y_pred, axes=[0])
+    return tf.sqrt(var)
+
+
+def hard_mining_mse(k):
+    """
+    Compute MSE for steering evaluation and hard-mining for the current batch.
+
+    # Arguments
+        k: number of samples for hard-mining.
+
+    # Returns
+        custom_mse: average MSE for the current batch.
+    """
+
+    def custom_mse(y_true, y_pred):
+        # Steering loss
+        l_steer = backend.square(y_pred - y_true)
+        l_steer = tf.squeeze(l_steer, axis=-1)
+
+        # Hard mining
+        k_min = tf.minimum(k, tf.shape(l_steer)[0])
+        _, indices = tf.nn.top_k(l_steer, k=k_min)
+        max_l_steer = tf.gather(l_steer, indices)
+        hard_l_steer = tf.divide(tf.reduce_sum(max_l_steer), tf.cast(k, tf.float32))
+
+        return hard_l_steer
+
+    return custom_mse
+
+
+def train_model(model: Model, train_data_generator: DataGenerator.CustomSequence, val_data_generator: DataGenerator.CustomSequence,
+                batch_size: int, learn_rate: float, initial_epoch: int, epochs: int):
+    lr_scale_factor = 0.5
+    log_rate = 10
+    experiment_rootdir = "checkpoints"
+    # Create the experiment rootdir if not already there
+    if not os.path.exists(experiment_rootdir):
+        os.makedirs(experiment_rootdir)
+
+    # Initialize number of samples for hard-mining
+    model.k_mse = tf.Variable(batch_size, trainable=False, name='k_mse', dtype=tf.int32)
+
+    # Configure training process
+    optimizer = keras.optimizers.Adam(learning_rate=learn_rate, decay=1e-4)
+    model.compile(loss=[hard_mining_mse(model.k_mse)], optimizer=optimizer, metrics=[steering_loss, pred_std])
+
+    # Save model with the lowest validation loss
+    weights_path = os.path.join(experiment_rootdir, 'weights_{epoch:03d}.h5')
+    write_best_model = ModelCheckpoint(filepath=weights_path, monitor='val_steering_loss', save_best_only=True, save_weights_only=True)
+
+    # Save model every 'log_rate' epochs.
+    # Save training and validation losses.
+    # logz.configure_output_dir(FLAGS.experiment_rootdir)
+    save_model_and_loss = log_utils.MyCallback(filepath=experiment_rootdir, period=log_rate, batch_size=batch_size, factor=lr_scale_factor)
+
+    # Train model
+    steps_per_epoch = np.minimum(int(np.ceil(train_data_generator.samples / batch_size)), 2000)
+    validation_steps = int(np.ceil(val_data_generator.samples / batch_size)) - 1
+
+    print("Train {}, vali {}, batch {}".format(train_data_generator.samples, val_data_generator.samples, batch_size))
+    print("Step per epoch {}".format(steps_per_epoch))
+    print("Validation steps {}".format(validation_steps))
+
+    model.fit(train_data_generator, batch_size=batch_size,
+              epochs=epochs, steps_per_epoch=steps_per_epoch,
+              callbacks=[write_best_model, save_model_and_loss],
+              validation_data=val_data_generator,
+              validation_steps=validation_steps,
+              initial_epoch=initial_epoch)
+
+
+def _main(flags: argparse) -> None:
+    channels_dict = {"grayscale": 1, "rgb": 3, "rgba": 4}
+    img_height, img_width = flags.img_height, flags.img_width
+    # img_channels = channels_dict["rgb"]
+    batch_size = flags.batch_size
+    learn_rage = flags.learning_rate
+    initial_epoch = 0  # Used to restart learning from checkpoint
+    epochs = flags.epochs
+
+    # Generate training data with real-time augmentation
+    if flags.frame_mode == "dvs":
+        img_channels = channels_dict["rgb"]
+
+    elif flags.frame_mode == "aps":
+        img_channels = channels_dict["grayscale"]
+
+    else:
+        img_channels = channels_dict["rgb"]
+
+    train_image_loader = DataGenerator.CustomSequence(flags.train_dir, flags.frame_mode, True, (img_height, img_width), batch_size, False)
+    val_image_loader = DataGenerator.CustomSequence(flags.train_dir, flags.frame_mode, False, (img_height, img_width), batch_size, False)
+
+    model = build_model(img_height, img_width, img_channels, 1)
+    train_model(model, train_image_loader, val_image_loader, batch_size, learn_rage, initial_epoch, epochs)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--train_dir", help="Folder containing training experiments", type=str, default=None)
+    parser.add_argument("-v", "--test_dir", help="Folder containing testing experiments", type=str, default=None)
+    parser.add_argument("-r", "--random_seed", help="Initial random seed", type=int, default=100)
+    parser.add_argument("-f", "--frame_mode", help="Load mode for images, either dvs, aps or aps_diff", type=str, default="dvs")
+    parser.add_argument("-b", "--batch_size", help="Batch size in training and evaluation", type=int, default=32)
+    parser.add_argument("-e", "--epochs", help="Number of epochs for training", type=int, default=100)
+    parser.add_argument("-l", '--learning_rate', help="Initial learning rate for adam", type=float, default=1e-4)
+    parser.add_argument("-iw", "--img_width", help="Target Image Width", type=int, default=200)
+    parser.add_argument("-ih", "--img_height", help="Target Image Height", type=int, default=200)
+    args = parser.parse_args()
+
+    if args.train_dir is None:
+        print("Missing --train_dir parameter")
+        exit(-1)
+
+    if args.test_dir is None:
+        print("Missing --test_dir parameter")
+        exit(-1)
+
+    if not os.path.exists(args.train_dir) or not os.path.isdir(args.train_dir):
+        print("--train_dir {} is not a directory".format(args.train_dir))
+        exit(-1)
+
+    if not os.listdir(args.train_dir):
+        print("--train_dir {} is empty".format(args.train_dir))
+        exit(-1)
+
+    if args.frame_mode not in ["dvs", "aps", "aps_diff"]:
+        print("A valid --frame_mode must be selected")
+        exit(-1)
+
+    _main(args)
