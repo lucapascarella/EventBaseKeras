Fix an issue in the data y normalization, testing with a small training

DataGenerator.py
@@ -5,8 +5,6 @@ import json
 import numpy as np
 
 from unipath import Path
-import tensorflow
-from tensorflow import keras
 from keras import backend
 from keras import Sequential
 from keras.utils import Sequence
@@ -14,8 +12,7 @@ from keras.layers import RandomRotation, RandomZoom, RandomTranslation, RandomBr
 from typing import Tuple
 
 import img_utils
-
-import matplotlib.pyplot as plt
+from utils import normalize_nparray
 
 
 class MyInput:
@@ -154,10 +151,18 @@ class CustomSequence(Sequence):
         ])
 
         self.samples = len(self.input_data)
-        print("Selected {} of {} frames for {}".format(self.samples, steering_datapoints, 'training' if is_training else 'validation'))
+        mode = 'training' if self.is_training else 'validation'
+        print("Selected {} of {} frames for {}".format(self.samples, steering_datapoints, mode))
         self.indexes = np.arange(self.samples)
         if self.shuffle:
             np.random.shuffle(self.indexes)
+
+        indexes_csv = os.path.join(Path(input_dir_data).parent, "indexes_{}.csv".format(mode))
+        with open(indexes_csv, 'w') as idx_file:
+            idx_file.write("filename,index,actual_steering,future_steering,actual_output,future_output\n")
+            for idx in self.indexes:
+                myi = self.input_data[idx]
+                idx_file.write("{},{},{},{},{},{}\n".format(myi.filename, idx, myi.actual_steering, myi.future_steering, myi.actual_output, myi.future_output))
 
     def _recursive_list(self, sub_path):
         return sorted(os.walk(sub_path, followlinks=self.follow_links), key=lambda tpl: tpl[0])
@@ -184,7 +189,7 @@ class CustomSequence(Sequence):
             clip_max = np.max(outputs)
 
             # Scaling all values
-            outputs /= np.max(np.abs(outputs), axis=0)
+            outputs = normalize_nparray(outputs, -1, 1)
 
             out_dict = {'means': means.tolist(),
                         'stds': stds.tolist(),
@@ -206,15 +211,11 @@ class CustomSequence(Sequence):
             outputs = np.clip(outputs, means - 3 * stds, means + 3 * stds)
 
             # Scaling of all values
-            mins = np.array(train_dict['mins'])
-            maxs = np.array(train_dict['maxs'])
+            data_min = train_dict['mins']
+            data_max = train_dict['maxs']
 
-            # Range of the transformed data
-            min_bound = -1.0
-            max_bound = 1.0
-
-            outputs = (outputs - mins) / (maxs - mins)
-            outputs = outputs * (max_bound - min_bound) + min_bound
+            # Scaling all values
+            outputs = normalize_nparray(outputs, -1, 1, data_min, data_max)
 
         return outputs
 
@@ -234,7 +235,7 @@ class CustomSequence(Sequence):
         if self.augment_data:
             # Emulate ImageDataGenerator => random_transform(x) and standardize(x)
             batch_x_aug = np.zeros((self.batch_size, self.image_size[0], self.image_size[1], self.image_size[2]), dtype=backend.floatx())
-            plt.figure(figsize=(10, 10))
+            # plt.figure(figsize=(10, 10))
             for i in range(batch_x.shape[0]):
                 # Does not work without passing the two seconds arguments
                 batch_x_aug[i] = self.data_augmentation(batch_x[i], {"flip_horizontal": False, "flip_vertical": False})

adversarial_steering.py
@@ -20,7 +20,6 @@ from keras.layers import Activation
 from keras.utils.generic_utils import get_custom_objects
 from tensorflow.python.keras import backend as kernel
 from keras.models import load_model
-from keras import losses
 
 from img_utils import save_steering_degrees
 
@@ -115,10 +114,10 @@ def show_sub_figs(title: str, images: List[Tuple[np.array, np.array]]):
     plt.show()
 
 
-def normalize_array(img: np.array, min_bound: float = 0, max_bound: float = 255) -> np.array:
-    val_range = img.max() - img.min()
-    tmp = (img - img.min()) / val_range
-    return tmp * (max_bound - min_bound) + min_bound
+def get_image_by_index(batch_size: int, img_index: int) -> Tuple[int, int]:
+    img_idx = img_index % batch_size
+    batch_idx = img_index // batch_size
+    return batch_idx, img_idx
 
 
 def _main(flags: argparse):
@@ -133,8 +132,8 @@ def _main(flags: argparse):
     target_model = load_custom_model(model_path, batch_size)
 
     # Select image to create an adversarial example from
-    x_batch, gt_steer_batch = image_loader.__getitem__(0)
-    img_idx = 105
+    batch_idx, img_idx = get_image_by_index(batch_size, 108)
+    x_batch, gt_steer_batch = image_loader.__getitem__(batch_idx)
     img = x_batch[img_idx:img_idx + 1]
     gt_steer = np.reshape(gt_steer_batch[img_idx:img_idx + 1], (1,))
     plt.imshow(img.reshape(img_shape), vmin=0., vmax=1.)
@@ -187,7 +186,7 @@ def _main(flags: argparse):
             print('Expected {:.1f}, predicted: {:.1f}'.format(y_gt, y_mp))
 
             if flags.frame_mode == "dvs":
-                flooded_img = normalize_array(flooded_img)
+                flooded_img = utils.normalize_nparray(flooded_img, 0, 255)
 
             img_filename = os.path.join(img_dir, "adversarial_{:03d}.png".format(idx))
             save_steering_degrees(img_filename, flooded_img, y_mp, y_gt, flags.frame_mode)

utils.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
+import numpy as np
 from keras import backend
 from keras.callbacks import History
 
@@ -39,6 +39,17 @@ def hard_mining_mse(k):
         return hard_l_steer
 
     return custom_mse
+
+
+def normalize_nparray(data: np.array, min_bound: float, max_bound: float, data_min: float = None, data_max: float = None) -> np.array:
+    if data_min is None:
+        data_min = data.min()
+    if data_max is None:
+        data_max = data.max()
+
+    val_range = data_max - data_min
+    tmp = (data - data_min) / val_range
+    return tmp * (max_bound - min_bound) + min_bound
 
 
 def plot_history(history: History, filename: str = None) -> None: